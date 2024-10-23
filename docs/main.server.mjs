e5014da17505f3f5e96a2c28643f973b9208e0bf
ACHRAF EL BADRI <www.achrafweb@gmail.com>
2024-10-23 20:34:10 +0200

    Files update

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-N36BTVBS.mjs";import"./chunk-737PKFMZ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZB6LOOCH.mjs";import"./chunk-737PKFMZ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
